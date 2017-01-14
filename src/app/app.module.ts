b03d84083a84787cce879ab91aa5ff2e20675e98
aniruddha <aniruddha.kudalkar@gmail.com>
2017-01-14 10:56:29 +0530

    view encapsulation and ng-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,23 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { FristComponent } from './frist/frist.component';
+import { OtherComponent } from './other/other.component';
+import { OtherOneComponent } from './other-one/other-one.component';
+import { InnerOneComponent } from './frist/inner-one.component';
+import { InnerTwoComponent } from './frist/inner-two.component';
+import { ViewEncapsulationComponent } from './view-encapsulation/view-encapsulation.component';
+import { ViewInComponent } from './view-encapsulation/view-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FristComponent
+    FristComponent,
+    OtherComponent,
+    OtherOneComponent,
+    InnerOneComponent,
+    InnerTwoComponent,
+    ViewEncapsulationComponent,
+    ViewInComponent
   ],
   imports: [
     BrowserModule,
