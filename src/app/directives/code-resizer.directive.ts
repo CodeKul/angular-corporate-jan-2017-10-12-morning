7320364a205d62675859ac784b9c12a1f0f8a24b
aniruddha <aniruddha.kudalkar@gmail.com>
2017-01-22 12:02:23 +0530

    getting events from host using HostListener and binding to attributes using HostBinding

--- src/app/directives/code-resizer.directive.ts
+++ src/app/directives/code-resizer.directive.ts
@@ -1,9 +1,24 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener, HostBinding, OnChanges } from '@angular/core';
 
 @Directive({
   selector: '[codeResizer]'
 })
-export class CodeResizerDirective {
+export class CodeResizerDirective implements OnChanges{
+
+  @HostBinding('style.width') 
+  widthBox : string;
+
+  @HostListener('mouseenter') 
+  mousein(){
+    //this.rend.setElementStyle(this.elRef.nativeElement, 'width', '300px');
+    this.widthBox = '300px';
+  }
+
+  @HostListener('mouseleave')
+  mouseout() {
+    //this.rend.setElementStyle(this.elRef.nativeElement, 'width', '200px');
+    this.widthBox = '200px';
+  }
 
   constructor(
     private elRef: ElementRef,
@@ -11,6 +26,11 @@ export class CodeResizerDirective {
   ) {
     //this.elRef.nativeElement.style.width='200px';
 
-    this.rend.setElementStyle(this.elRef.nativeElement, 'width', '300px');
+    // this.rend.setElementStyle(this.elRef.nativeElement, 'width', '200px');
+   // this.rend.setElementStyle(this.elRef.nativeElement, 'width', this.widthBox);
+  }
+
+  ngOnChanges() {
+    //this.rend.setElementStyle(this.elRef.nativeElement, 'width', this.widthBox + 'px');
   }
 }
