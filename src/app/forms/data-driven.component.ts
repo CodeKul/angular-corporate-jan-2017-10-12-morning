c7f93f3d57c4cbe4ca088ebc9e1bbfb454354fff
aniruddha <aniruddha.kudalkar@gmail.com>
2017-02-18 12:52:50 +0530

    custom validators

--- src/app/forms/data-driven.component.ts
+++ src/app/forms/data-driven.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, NgForm, Validators, FormArray } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  NgForm,
+  Validators,
+  FormArray,
+  ValidatorFn,
+  AbstractControl
+} from '@angular/forms';
 
 @Component({
   selector: 'app-data-driven',
@@ -16,14 +25,14 @@ export class DataDrivenComponent implements OnInit {
   constructor(formBuilder: FormBuilder) {
     this.myForm = formBuilder.group({
       userGroup: formBuilder.group({
-        userName: [''],
+        userName: ['', this.sampleValidatorV2('userName')],
         email: ['', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]]
       }),
-      password: ['', Validators.required],
-      country : ['India'],
-      nums : formBuilder.array([
+      password: ['', [Validators.required, this.sampleValidatorV2('password')]],
+      country: ['India'],
+      nums: formBuilder.array([
         new FormControl('9762548833'),
-        new FormControl('9762548834')        
+        new FormControl('9762548834')
       ])
     });
   }
@@ -37,5 +46,22 @@ export class DataDrivenComponent implements OnInit {
 
   addNewNum() {
     (<FormArray>this.myForm.controls['nums']).push(new FormControl())
+  }
+
+  sampleValidator(fc: FormControl): { [s: string]: boolean } {
+    return fc.value === 'a' ? { sample: true } : null;
+  }
+
+  sampleValidatorV2(name: string): ValidatorFn {
+    if (name === 'userName') {
+      return (control: AbstractControl): { [key: string]: any } => {
+        return control.value == 'a' ? { validation: true } : null;
+      };
+    }
+    if(name === 'password' ) {
+      return (control: AbstractControl): { [key: string]: any } => {
+        return control.value == 'b' ? { validation: true } : null;
+      };
+    }
   }
 }
